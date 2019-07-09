e578ce6efc886762f904b8f482f67f575f892020
omaru <omarux@gmail.com>
2019-07-09 15:45:28 -0600

    #3 adding department hateoas and services support

--- src/main/java/com/omaru/employee/resource/DepartmentResourceAssembler.java
+++ src/main/java/com/omaru/employee/resource/DepartmentResourceAssembler.java
@@ -4,6 +4,9 @@ import com.omaru.employee.controller.DepartmentController;
 import com.omaru.employee.domain.model.Department;
 import org.springframework.hateoas.mvc.ResourceAssemblerSupport;
 
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
 public class DepartmentResourceAssembler extends ResourceAssemblerSupport<Department, DepartmentResource> {
 
     public DepartmentResourceAssembler() {
@@ -12,6 +15,8 @@ public class DepartmentResourceAssembler extends ResourceAssemblerSupport<Depart
 
     @Override
     public DepartmentResource toResource(Department department) {
-        return new DepartmentResource(department);
+        DepartmentResource resource = new DepartmentResource(department);
+        resource.add(linkTo(methodOn(DepartmentController.class).getDepartment(department.getId())).withSelfRel());
+        return resource;
     }
 }
